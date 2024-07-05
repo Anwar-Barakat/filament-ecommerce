56602874c564f74bd22d52639d5d294a7fef2050
Anwar-Barakat <brkatanwar0@gmail.com>
2024-07-05 16:37:12 +0400

    feat: Add product form with validation and file upload functionality

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -2,17 +2,18 @@
 
 namespace App\Filament\Resources;
 
-use App\Enum\ProductTypeEnum;
-use App\Filament\Resources\ProductResource\Pages;
-use App\Filament\Resources\ProductResource\RelationManagers;
-use App\Models\Product;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use App\Enum\ProductTypeEnum;
+use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProductResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
 
 class ProductResource extends Resource
 {
@@ -30,19 +31,51 @@ class ProductResource extends Resource
             ->schema([
                 Forms\Components\Group::make()->schema([
                     Forms\Components\Section::make('Main Info')->schema([
-                        Forms\Components\TextInput::make('name')->autofocus()->required(),
-                        Forms\Components\TextInput::make('slug'),
+                        Forms\Components\TextInput::make('name')
+                            ->autofocus()
+                            ->live(onBlur: true)
+                            ->unique()
+                            ->placeholder('Enter product name')
+                            ->required()
+                            ->afterStateUpdated(function (string $operation, $state, Forms\Set $set) {
+                                if ($operation !== 'create') {
+                                    return;
+                                }
+                                $set('slug', Str::slug($state));
+                            }),
+
+                        Forms\Components\TextInput::make('slug')
+                            ->disabled()
+                            ->dehydrated()
+                            ->required()
+                            ->unique(Product::class, 'slug', ignoreRecord: true),
+
                         Forms\Components\MarkdownEditor::make('description')->columnSpan(2),
                     ])->columns(2),
 
                     Forms\Components\Section::make('Pricing & Inventory')->schema([
-                        Forms\Components\TextInput::make('sku')->autofocus()->required(),
-                        Forms\Components\TextInput::make('price'),
-                        Forms\Components\TextInput::make('quantity'),
+                        Forms\Components\TextInput::make('sku')
+                            ->label('SKU (Stock Keeping Unit)')
+                            ->placeholder('Enter product SKU')
+                            ->unique()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('price')
+                            ->numeric()
+                            ->rules('regex:/^\d{1,6}(\.\d{0,2})?$/')
+                            ->required(),
+
+                        Forms\Components\TextInput::make('quantity')
+                            ->numeric()
+                            ->minValue(0)
+                            ->maxValue(100)
+                            ->required(),
+
                         Forms\Components\Select::make('type')->options([
                             'downloadable' => ProductTypeEnum::DOWNLOADABLE->value,
                             'deliverable' => ProductTypeEnum::DELIVERABLE->value,
-                        ]),
+                        ])->required(),
+
 
                     ])->columns(2),
                 ]),
@@ -66,6 +99,10 @@ class ProductResource extends Resource
 
                     Forms\Components\Section::make('Attachments')->schema([
                         Forms\Components\FileUpload::make('image')
+                            ->directory('form-attachments')
+                            ->preserveFilenames()
+                            ->image()
+                            ->imageEditor()
                             ->required(),
                     ])->collapsible(),
 
@@ -85,16 +122,49 @@ class ProductResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\ImageColumn::make('image'),
-                Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('brand.name'),
-                Tables\Columns\IconColumn::make('is_visible')->boolean(),
-                Tables\Columns\TextColumn::make('price'),
-                Tables\Columns\TextColumn::make('quantity'),
-                Tables\Columns\TextColumn::make('published_at'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('brand.name')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\IconColumn::make('is_visible')
+                    ->boolean()
+                    ->sortable()
+                    ->toggleable()
+                    ->label('Visibility'),
+
+                Tables\Columns\TextColumn::make('price')
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\TextColumn::make('quantity')
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\TextColumn::make('published_at')
+                    ->sortable()
+                    ->toggleable()
+                    ->date(),
+
                 Tables\Columns\TextColumn::make('type'),
             ])
             ->filters([
-                //
+                Tables\Filters\TernaryFilter::make('is_visible')
+                    ->label('Visibility')
+                    ->boolean()
+                    ->trueLabel('Visible Products')
+                    ->falseLabel('Hidden Products')
+                    ->native(false),
+
+                Tables\Filters\SelectFilter::make('brand')
+                    ->relationship('brand', 'name')
+                    ->label('Brand')
+                    ->options(fn () => \App\Models\Brand::pluck('name', 'id')->toArray()),
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
